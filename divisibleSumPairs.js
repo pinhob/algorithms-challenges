530fe44cac1c7fec5dd8c8e556f02244df5ccba5
Bruno Pinho <brunopinho@outlook.com>
2022-06-08 14:46:50 -0300

    feat: divisibleSumPairs

---
+++ divisibleSumPairs.js
@@ -0,0 +1,25 @@
+// CHALLENGE: https://www.hackerrank.com/challenges/divisible-sum-pairs/
+
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+function divisibleSumPairs(n, k, ar) {
+  let result = 0;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        result += 1;
+      }
+    }
+  }
+
+  return result;
+}
