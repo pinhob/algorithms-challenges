9337d5208c040d106a6d89d25be5caa4461f5b60
Bruno Pinho <20286747+pinhob@users.noreply.github.com>
2024-10-05 22:16:19 -0300

    Create convertTheTemperature.js

---
+++ convertTheTemperature.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/convert-the-temperature/
+
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    return [celsius + 273.15, celsius * 1.80 + 32.00]
+};
